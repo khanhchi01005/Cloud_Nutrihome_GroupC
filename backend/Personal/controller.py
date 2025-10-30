1f096f3c3b6e323f7e9a3484c5c0625baf7eb663
Chi Khanh <khanhchi01005@gmail.com>
2025-10-30 11:38:09 +0700

    get personal data

---
+++ backend/Personal/controller.py
@@ -0,0 +1,11 @@
+from flask import Blueprint
+from .services import show_personal_detail
+from .services import update_personal_detail
+from .services import show_nutrition_today
+from .services import show_history
+
+personal = Blueprint("personal", __name__)
+
+@personal.route("/api/personal", methods =['GET'])
+def show_detail():
+    return show_personal_detail()
---
+++ backend/Personal/services.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify
+import sqlite3
+import datetime
+import json 
+import logging
+import os
+
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger(__name__)
+
+DATABASE = os.path.join(os.path.dirname(os.getcwd()), 'nutrihome.db')
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+#Show personal detail 
+def show_personal_detail():
+    data = request.json 
+    user_id = data.get('user_id')
+    conn = get_db_connection()
+    person = conn.execute(
+    '''SELECT fullname,
+            user_id,
+            username,
+            gender,
+            weight,
+            height,
+            dob,
+            avatar,
+            activity_level,
+            disease,
+            allergen   
+    FROM users WHERE user_id = ?''',(user_id,)).fetchone ()
+    conn.close()
+    
+    if person:
+         return jsonify({
+            'status': 'success',
+            'data': {
+                'user_id': person['user_id'],
+                'avatar': person['avatar'],
+                'fullname': person['fullname'],
+                'username': person['username'],
+                'gender': person['gender'],
+                'dob': person['dob'],
+                'height': person['height'],
+                'weight': person['weight'],
+                'activity_level': person['activity_level'],
+                'disease': person['disease'],
+                'allergen': person['allergen']
+            }
+        }), 200, {'Content-Type': 'application/json'}
+    else:
+        return jsonify({'status': 'error', 'message': 'Unavailable user'}), 404
